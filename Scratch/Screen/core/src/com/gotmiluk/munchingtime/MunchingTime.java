6860b5c2798f776322f8561f028029f7d2043e7d
Justin <goebj0742@wrdsb.ca>
2018-09-25 12:21:10 -0400

    Movement in progress

--- Scratch/Screen/core/src/com/gotmiluk/munchingtime/MunchingTime.java
+++ Scratch/Screen/core/src/com/gotmiluk/munchingtime/MunchingTime.java
@@ -1,9 +1,11 @@
 package com.gotmiluk.munchingtime;
 
 import com.badlogic.gdx.Game;
+import com.gotmiluk.munchingtime.ScrPlay;
 
 public class MunchingTime extends Game {
     ScrMenu scrMenu;
+    ScrPlay scrPlay;
 
 
     int nScreen; // 0 for menu, 1 for play
@@ -12,7 +14,10 @@ public class MunchingTime extends Game {
         nScreen = _nScreen;
         if (nScreen == 0) {
             setScreen(scrMenu);
+        } else if (nScreen == 1) {
+            setScreen(scrPlay);
         }
+
         }
 
 
@@ -21,7 +26,7 @@ public class MunchingTime extends Game {
         nScreen = 0;
         // notice that "this" is passed to each screen. Each screen now has access to methods within the "game" master program
         scrMenu = new ScrMenu(this);
-
+        scrPlay = new ScrPlay(this);
 
         updateState(0);
     }
